0e8bbad53a320341d324ca8ab8a94dce009e5e8a
meng zhou <mengzhou44@gmail.com>
2019-12-22 20:26:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import _ from 'lodash';
+import faker from 'faker';
+import Pagination from './pagination';
 
-function App() {
+export default function App() {
+  const  items =getItems();
+  const [pageItems, setPageItems] = useState(items.slice(1,11));
+  
+  function getItems() {
+    let array = [];
+    for (var i = 1; i < 1000; i++) {
+      array.push({
+        id: i,
+        name: faker.name.findName()
+      });
+    }
+    return array; 
+  }
+  
+  function renderPageItem(item) {
+    return <li key={item.id}>{item.name}</li>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>{_.map(pageItems, renderPageItem)}</ul>
+
+      <Pagination
+        items={items}
+        onChangePage={pageItems => {
+          setPageItems(pageItems);
+        }
+      }
+      />
     </div>
   );
 }
-
-export default App;
---
+++ src/pagination.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import styles from './pagination.module.scss';
+
+export default function Pagination({
+  items,
+  onChangePage,
+  initialPage = 1,
+  pageSize = 10
+}) {
+  const [paginator, setPaginator] = useState({});
+
+  useEffect(() => {
+    if (items && items.length) {
+      debugger;
+      setPaginator(getPaginator(items, initialPage, pageSize));
+    }
+  }, []);
+
+  function setPage(page) {
+    if (page > 0 && page <= paginator.totalPages) {
+      const newPaginator = getPaginator(items, page, pageSize);
+
+      setPaginator(newPaginator);
+      onChangePage(newPaginator.pageItems);
+    }
+  }
+
+  if (!paginator.pages || paginator.pages.length <= 1) {
+    return <Fragment />;
+  }
+
+  return (
+    <ul className={styles.pagination}>
+      <li
+        className={paginator.currentPage === 1 ? styles.disabled : ''}
+        onClick={() => setPage(paginator.currentPage - 1)}
+      >
+        <span>{'<'}</span>
+      </li>
+      {paginator.pages.map((page, index) => (
+        <li
+          key={index}
+          onClick={() => setPage(page)}
+          className={paginator.currentPage === page ? styles.active : ''}
+        >
+          <span>{page}</span>
+        </li>
+      ))}
+      <li
+        onClick={() => setPage(paginator.currentPage + 1)}
+        className={
+          paginator.currentPage === paginator.totalPages ? styles.disabled : ''
+        }
+      >
+        <span>{'>'}</span>
+      </li>
+    </ul>
+  );
+}
+
+function getPaginator(items, currentPage, pageSize) {
+  currentPage = currentPage || 1;
+  const totalPages = Math.ceil(items.length / pageSize);
+
+  let startPage, endPage;
+  if (totalPages <= 10) {
+    startPage = 1;
+    endPage = totalPages;
+  } else {
+    if (currentPage <= 6) {
+      startPage = 1;
+      endPage = 10;
+    } else if (currentPage + 4 >= totalPages) {
+      startPage = totalPages - 9;
+      endPage = totalPages;
+    } else {
+      startPage = currentPage - 5;
+      endPage = currentPage + 4;
+    }
+  }
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = Math.min(startIndex + pageSize - 1, items.length - 1);
+  const pages = [...Array(endPage + 1 - startPage).keys()].map(
+    i => startPage + i
+  );
+  const pageItems = items.slice(startIndex, endIndex + 1);
+
+  return {
+    currentPage,
+    totalPages,
+    pages,
+    pageItems
+  };
+}
